e73173cfd5848e61d65d01d841e33f29c5826e01
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-10-20 21:12:42 -0300

    mapping task

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -3,6 +3,26 @@ pragma solidity ^0.8.6;
 
 contract TaskContract{
 
+    uint taskCounter = 0;
+
+
+    struct Task {
+        
+        uint id;
+        string title;
+        string description;
+        bool done;
+        uint createdAt;
+
+    }
+
+    mapping (uint => Task) public tasks;
+
+    function createTask(string memory _title , string memory _description) public
+    {
+        tasks[1] = Task(taskCounter, _title, _description);    
+    }
+    // function toggleDone(){}
 }
 
 
